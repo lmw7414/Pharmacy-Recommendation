55f17cebe395207011e6cece4cb23011a808c8d4
Lee Min Woo <lmw7414@naver.com>
2023-05-26 15:55:30 +0900

    #8 - feat : pharmacy -> pharmacyDto 변환 메서드 및 약국 리스트 검색 메서드 생성
    약국 레포지토리에 저장된 약국 검색 후 dto 리스트로 변환해주는 메서드 생성

---
+++ src/main/java/com/example/pharmacyrecommendation/pharmacy/service/PharmacySearchService.java
@@ -0,0 +1,39 @@
+package com.example.pharmacyrecommendation.pharmacy.service;
+
+import com.example.pharmacyrecommendation.pharmacy.dto.PharmacyDto;
+import com.example.pharmacyrecommendation.pharmacy.entity.Pharmacy;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacySearchService {
+
+    private final PharmacyRepositoryService pharmacyRepositoryService;
+
+    public List<PharmacyDto> searchPharmacyDtoList() {
+        //TODO: 추후 redis도 지원
+
+        //db
+        return pharmacyRepositoryService.findAll()
+                .stream()
+                .map(this::convertToPharmacyDto)
+                .collect(Collectors.toList());
+    }
+
+    private PharmacyDto convertToPharmacyDto(Pharmacy pharmacy) {
+        return PharmacyDto.builder()
+                .id(pharmacy.getId())
+                .pharmacyAddress(pharmacy.getPharmacyAddress())
+                .pharmacyName(pharmacy.getPharmacyName())
+                .latitude(pharmacy.getLatitude())
+                .longitude(pharmacy.getLongitude())
+                .build();
+    }
+
+}
