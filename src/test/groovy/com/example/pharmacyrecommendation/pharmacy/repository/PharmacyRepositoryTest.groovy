19404e7cf1f34032efc7652a61c8d9a2ff1c61c2
Lee Min Woo <lmw7414@naver.com>
2023-05-26 14:55:30 +0900

    #11 - bug : @EnableJpaAuditing 추가

--- src/test/groovy/com/example/pharmacyrecommendation/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/pharmacyrecommendation/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -63,7 +63,7 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
     def "BaseTimeEntity 등록"() {
         given:
-        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now()
         String address = "서울 특별시 성북구 종암동"
         String name = "은혜 약국"
 
@@ -71,12 +71,13 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
                 .pharmacyAddress(address)
                 .pharmacyName(name)
                 .build()
+
         when:
         pharmacyRepository.save(pharmacy)
-        def result = pharmacyRepository.findAll();
+        def result = pharmacyRepository.findAll()
 
         then:
-        result.get(0).getCreatedDate().isAfter(now);
-        result.get(0).getModifiedDate().isAfter(now);
+        result.get(0).getCreatedDate().isAfter(now)
+        result.get(0).getModifiedDate().isAfter(now)
     }
 }
