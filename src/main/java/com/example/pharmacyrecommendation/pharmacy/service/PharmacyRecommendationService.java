ed1c8ee311885f98b9e50bc0373dd36efc2d06e8
Lee Min Woo <lmw7414@naver.com>
2023-05-30 14:04:45 +0900

    #15 - feat: 약국 추천결과 저장 메서드 기능 구현
    사용자 주소를 위, 경도로 변환 후, 해당 위치를 기준으로 가까운 약국을 찾고 저장하는 로직 개발.

---
+++ src/main/java/com/example/pharmacyrecommendation/pharmacy/service/PharmacyRecommendationService.java
@@ -0,0 +1,35 @@
+package com.example.pharmacyrecommendation.pharmacy.service;
+
+import com.example.pharmacyrecommendation.api.dto.DocumentDto;
+import com.example.pharmacyrecommendation.api.dto.KakaoApiResponseDto;
+import com.example.pharmacyrecommendation.api.service.KakaoAddressSearchService;
+import com.example.pharmacyrecommendation.direction.entity.Direction;
+import com.example.pharmacyrecommendation.direction.service.DirectionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacyRecommendationService {
+
+    private final KakaoAddressSearchService kakaoAddressSearchService;
+    private final DirectionService directionService;
+
+    public void recommendPharmacyList(String address) {
+        KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
+
+        if(Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
+            log.error("[PharmacyRecommendationService recommendPharmacyList fail] Input address: {}", address);
+            return;
+        }
+        DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
+        List<Direction> directionList = directionService.buildDirectionList(documentDto);
+        directionService.saveAll(directionList);
+    }
+}
