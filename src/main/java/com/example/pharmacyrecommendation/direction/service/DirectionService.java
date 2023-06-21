9ac866ae85d37aceb7be217ff49fa385c1a93c3f
Lee Min Woo <lmw7414@naver.com>
2023-06-21 11:49:48 +0900

    #21 - feat: 실제 길안내 url로 변경하는 메서드 추가

--- src/main/java/com/example/pharmacyrecommendation/direction/service/DirectionService.java
+++ src/main/java/com/example/pharmacyrecommendation/direction/service/DirectionService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.transaction.Transactional;
 import java.util.Collections;
@@ -24,16 +25,31 @@ public class DirectionService {
 
     private static final int MAX_SEARCH_COUNT = 3;
     private static final double RADIUS_KM = 10.0;
+    private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
+
 
     private final PharmacySearchService pharmacySearchService;
     private final DirectionRepository directionRepository;
     private final KakaoCategorySearchService kakaoCategorySearchService;
+    private final Base62Service base62Service;
 
     @Transactional
     public List<Direction> saveAll(List<Direction> directions) {
         if (CollectionUtils.isEmpty(directions)) return Collections.emptyList();
 
         return directionRepository.saveAll(directions);
+    }
+
+    public String findDirectionUrlById(String encodedId) {
+        Long decodedId = base62Service.decodedDirectionId(encodedId);
+        Direction direction = directionRepository.findById(decodedId).orElse(null);
+
+        String params = String.join(",", direction.getTargetPharmacyName(),
+                String.valueOf(direction.getTargetLatitude()),
+                String.valueOf(direction.getTargetLongitude())
+        );
+        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params).toUriString();
+        return result;
     }
 
     public List<Direction> buildDirectionList(DocumentDto documentDto) {
